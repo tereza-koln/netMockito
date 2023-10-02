8773ca7cc2312710cec5b5db12580492df98cb2d
Tereza <ruanapoor@gmail.com>
2023-10-02 07:08:40 +0300

    New

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -82,4 +82,22 @@ public class ManagerTest {
 
 
     }
+
+    @Test
+    public void testLast7() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+        manager.addMovie("Film 7");
+
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Film 7", "Film 6", "Film 5", "Film 4", "Film 3"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
